ba2f7039f2b32072ae3a95f66ab3122e4aa9b882
tejaswi <tejaswi.kundur@tanla.com>
2021-10-07 12:59:57 +0530

    cpu and memory profiling

---
+++ profiling/kafka_protobuff/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"net/http"
+	_ "net/http/pprof"
+
+	"google.golang.org/protobuf/proto"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+	// defer profile.Start().Stop()
+	// defer profile.Start(profile.MemProfile).Stop()
+	elliot := &Person{
+		Name: "Elliot",
+		Age:  25,
+	}
+	data, err := proto.Marshal(elliot)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	fmt.Println(data)
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+	topic := "test-consumer"
+
+	deliveryChan := make(chan kafka.Event)
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          data,
+	}, deliveryChan)
+
+	select {
+	case ev := <-deliveryChan:
+		switch e := ev.(type) {
+		case *kafka.Message:
+			if e.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", e.TopicPartition)
+			} else {
+				fmt.Printf("Delivered message to %v\n", e.TopicPartition)
+				fmt.Printf("Delivered message to %v\n", string(e.Value))
+			}
+			close(deliveryChan)
+		}
+	}
+}
